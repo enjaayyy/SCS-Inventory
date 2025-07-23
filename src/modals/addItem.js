90ea615098407ab0b8d3052da875b5cd8daf565e
enjaayyy <enujeemontesaarubeosu@gmail.com>
2025-07-23 17:41:49 +0800

    finished add item modal

--- src/modals/addItem.js
+++ src/modals/addItem.js
@@ -10,9 +10,35 @@ export default function AddItem({ onClose}){
           </div>
           <p>Add item to the inventory</p>
           <div className="modal-body">
-
+            <div className="image-container">
+              <p className="labels">Upload Image</p>
+              <label htmlFor ="image-upload" className="image-upload">
+                <div className="upload-container">
+                  <div>
+                    <img src="/images/upload-image.svg"></img>
+                    <p>Upload File</p>
+                  </div>
+                </div>
+              </label>
+              <input type="file" accept="image/*" id="image-upload" hidden></input>
+            </div>
+            <div className="data-container">
+              <p className="labels">Product Name</p>
+              <input className="product-name-input"></input>
+              <p className="labels">Location</p>
+              <select className="product-select-input"
+              onChange={(e) => console.log(e.target.value)}>
+                <option value="awda">awda</option>
+                <option value="23">23</option>
+              </select>
+              <p className="labels">Qty.</p>
+              <input className="product-qty-input" type="number"></input>
+              <div className="submit-btn-container">
+                <button className="submit-btn">Submit</button>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
-      </div>
     );
 }
